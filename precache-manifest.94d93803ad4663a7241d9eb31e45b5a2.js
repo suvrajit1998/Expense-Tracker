232744e1855e932427a6cc02a8377bdb46324b2e
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-02-24 16:40:51 +1400

    Updates

---
+++ precache-manifest.94d93803ad4663a7241d9eb31e45b5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b34b4f7dcc63b91eb1344c255c9714",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "e1b325372dbab32e55b2",
+    "url": "/Expense-Tracker/static/css/main.3dfe2a1a.chunk.css"
+  },
+  {
+    "revision": "ddce3ef05009be77ac45",
+    "url": "/Expense-Tracker/static/js/2.e543543b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Expense-Tracker/static/js/2.e543543b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b325372dbab32e55b2",
+    "url": "/Expense-Tracker/static/js/main.e22773d7.chunk.js"
+  },
+  {
+    "revision": "b769e279a0cd11d8c7db",
+    "url": "/Expense-Tracker/static/js/runtime-main.b92ef8d0.js"
+  }
+]);
